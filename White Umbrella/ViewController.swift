856970054e50ba149c821ed096580bcf43ffa434
Alexander <a.pride.xxi@tutanota.com>
2022-05-18 15:43:08 +0300

    Initial Commit

---
+++ White Umbrella/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  White Umbrella
+//
+//  Created by Александр Прайд on 18.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .link
+    }
+
+
+}
+
